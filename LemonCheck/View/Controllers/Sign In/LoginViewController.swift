83bef1398eeffcd1a131066258cd7eebd39ff2db
Biz <biz@varleyparker.com>
2021-01-11 07:07:37 +0000

    Enabled sign in with apple via dev account

--- LemonCheck/View/Controllers/Sign In/LoginViewController.swift
+++ LemonCheck/View/Controllers/Sign In/LoginViewController.swift
@@ -29,8 +29,8 @@ class LoginViewController: UIViewController {
         let request = ASAuthorizationAppleIDProvider().createRequest()
         request.requestedScopes = [.fullName, .email]
         let controller = ASAuthorizationController(authorizationRequests: [request])
-        //controller.delegate = self
-        //controller.presentationContextProvider = self
+        controller.delegate = self
+        controller.presentationContextProvider = self
         controller.performRequests()
     }
     
@@ -75,5 +75,72 @@ class LoginViewController: UIViewController {
      // Pass the selected object to the new view controller.
      }
      */
+    
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    
+    private func registerNewAccount(credential: ASAuthorizationAppleIDCredential){
+        print("Registering new account with user: \(credential.user)")
+        delegate?.didFinishAuth()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func signInWithExistingAccount(credential: ASAuthorizationAppleIDCredential){
+        print("Sign in with existing user: \(credential.user)")
+        delegate?.didFinishAuth()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func signInWithUserAndPassword(credential: ASPasswordCredential){
+        print("Signing in using an existing iCloud keychain credential: \(credential.user)")
+        delegate?.didFinishAuth()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        
+        case let appleIdCredential as ASAuthorizationAppleIDCredential:
+            let userId = appleIdCredential.user
+            UserDefaults.standard.set(userId, forKey: SignInWithAppleManager.userIdentifierKey)
+            
+            if let _ = appleIdCredential.email, let _ = appleIdCredential.fullName {
+                registerNewAccount(credential: appleIdCredential)
+            } else {
+                signInWithExistingAccount(credential: appleIdCredential)
+            }
+            
+            break
+            
+        case let passwordCredential as ASPasswordCredential:
+            let userId = passwordCredential.user
+            UserDefaults.standard.set(userId, forKey: SignInWithAppleManager.userIdentifierKey)
+            
+            signInWithUserAndPassword(credential: passwordCredential)
+            
+            break
+            
+        default:
+            break
+            }
+        }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    
     
 }
