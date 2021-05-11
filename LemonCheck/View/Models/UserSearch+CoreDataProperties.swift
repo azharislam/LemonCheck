ac0db9cfa5cc53f719be0df28efd8a0ad024aee4
Kazi Abdullah Al Mamun <kazi.a.mamun@gmail.com>
2021-05-12 05:09:02 +0600

    core data managed file added, project run on simulator problem solved

--- LemonCheck/View/Models/UserSearch+CoreDataProperties.swift
+++ LemonCheck/View/Models/UserSearch+CoreDataProperties.swift
@@ -1,10 +1,12 @@
 //
 //  UserSearch+CoreDataProperties.swift
-//  
+//  LemonCheck
 //
-//  Created by Azhar on 15/03/2021.
+//  Created by Kazi Abdullah Al Mamun on 5/12/21.
+//  Copyright © 2021 Varley Parker. All rights reserved.
 //
 //
+
 
 import Foundation
 import CoreData
@@ -16,22 +18,26 @@ extension UserSearch {
         return NSFetchRequest<UserSearch>(entityName: "UserSearch")
     }
 
-    @NSManaged public var vrm: String?
+    @NSManaged public var colour: String?
+    @NSManaged public var financeRecordCount: String?
+    @NSManaged public var financeRecordList: [String]?
+    @NSManaged public var importDate: String?
+    @NSManaged public var isFinanced: Bool
+    @NSManaged public var isImported: Bool
+    @NSManaged public var isScrapped: Bool
+    @NSManaged public var isWrittenOff: Bool
     @NSManaged public var make: String?
     @NSManaged public var model: String?
-    @NSManaged public var year: String?
-    @NSManaged public var colour: String?
     @NSManaged public var previousKeeperCount: String?
-    @NSManaged public var isWrittenOff: Bool
+    @NSManaged public var scrapDate: String?
+    @NSManaged public var stolenInfoSource: String?
+    @NSManaged public var vrm: String?
     @NSManaged public var writeOffCategory: String?
     @NSManaged public var writeOffDate: String?
-    @NSManaged public var financeRecordCount: String?
-    @NSManaged public var isFinanced: Bool
-    @NSManaged public var isScrapped: Bool
-    @NSManaged public var scrapDate: String?
-    @NSManaged public var isImported: Bool
-    @NSManaged public var importDate: String?
-    @NSManaged public var stolenInfoSource: String?
-    @NSManaged public var financeRecordList: [String]?
+    @NSManaged public var year: String?
+
+}
+
+extension UserSearch : Identifiable {
 
 }
